fb7575bb27f82a4a6fdb946de0568108e981e3ce
mhuang7100 <mhuang00@ladueschools.net>
2023-04-19 23:00:18 +0000

    All score calculations done

--- YahtzeeDice.java
+++ YahtzeeDice.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 
-import javax.xml.namespace.QName;
-
 // dice 
 public class YahtzeeDice {
     private static ArrayList<Integer> keep = new ArrayList<Integer>();
-    private static int[] dice = {5, 2, 5, 5, 2};
+    private static int[] dice = {2, 1, 2, 2, 2};
 
     public static void main(String[] args){
         printDice();
 
-        System.out.println(calcFullHouse());
+        System.out.println(calcYahtzee());
     }
 
     public static void keepDice(int index){
@@ -88,8 +86,9 @@ public class YahtzeeDice {
         return 0;
     }
 
+    // checks for full house and returns score
     public static int calcFullHouse(){
-        int total = 0;
+        int score = 25;
         int count = 0;
         int count2 = 0;
         int temp = 0;
@@ -98,7 +97,6 @@ public class YahtzeeDice {
             if (i == dice[0]){
                 count += 1;
             } else temp = i; 
-            total += i;
         }
         // if its not a full house, no score
         if (count == 1 || count >= 4){
@@ -110,17 +108,100 @@ public class YahtzeeDice {
             }
         }
         if (count2 + count == 5){
-            return total;
+            return score;
         } else return 0;
     }
 
-    
+    // checks for small straight (sequence of 4) and returns score
+    public static int calcSmStraight(){
+        int score = 30;
+        int count = 1;
+        int min = 6;
+        int minNext = 6;
 
-    public ArrayList<Integer> getKeep(){
+        // gets the 2 minimum values
+        for (int i : dice){
+            if (i < min){
+                min = i;
+            } else if (i < minNext){
+                minNext = i;
+            }
+        }
+        for (int i = 2; i <= 4; i++){
+            for (int j : dice){
+                if (j == min + count){
+                    count += 1;
+                }
+            }
+            if (count != i){
+                break;
+            }
+        }
+        
+        if (count >= 4){
+            return score;
+        } else {
+            count = 1;
+            for (int i = 2; i <= 4; i++){
+                for (int j : dice){
+                    if (j == minNext + count){
+                        count += 1;
+                    }
+                }
+                if (count != i){
+                    return 0;
+                }
+            }
+            if (count >= 4){
+                return score;
+            }
+        }
+
+        return 0;
+    }
+
+    // checks for large straight (sequence of 5) and returns score
+    public static int calcLgStraight(){
+        int score = 40;
+        int count = 1;
+        int min = 3;
+
+        // gets the min value
+        for (int i : dice){
+            if (i < min){
+                min = i;
+            }
+        }
+        for (int i = 2; i <= 5; i++){
+            for (int j : dice){
+                if (j == min + count){
+                    count += 1;
+                    break;
+                }
+            }
+            if (count != i){
+                return 0;
+            }
+        }
+
+        return score;
+    }
+
+    public static int calcYahtzee(){
+        int score = 50; 
+        for (int i : dice){
+            if (i != dice[0]){
+                return 0;
+            }
+        }
+        return score;
+    }
+
+    public static ArrayList<Integer> getKeep(){
         return keep;
     }
 
-    public int[] getDice(){
+    public static int[] getDice(){
         return dice;
     }
 }
